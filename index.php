0cfd8f0ff7209392e5b8e0aad75b3db1cc53fa31
raghavheda <40068511+raghavheda@users.noreply.github.com>
2018-09-24 11:28:39 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -15,26 +15,25 @@
     </header>
     <div class="content">
         <a class="icon patient_register" href="purchaseDept.php" >
-		<div><br><img src="images/patient.png"></div>
+		<div><br><img src="Images/purchase.jpg" alt="Smiley face" height="150" width="150"> </div>
             <div class="logo pRegisterl"></div>
             <div class="title pRegistert">Purchase Department</div>
         </a>
         <a class="icon hospital_register" href = "stakeholder.php">
-		<div><br><img src="images/hospital.png"></div>
+		<div><br><img src="Images/stake.jpg"alt="Smiley face" height="150" width="150"> </div>
             <div class="logo hRegisterl"></div>
             <div class="title hRegistert"> Stakeholders</div>
         </a>
         <a class="icon doctor_register" href = "StorageDept.php">
-		    <div><br><img src="images/doctor.png"></div>
+		    <div><br><img src="Images/store.jpg" height="150" width="150"> </div>
             <div class="logo d_registerl"></div>
             <div class="title d_registert">Storage Department</div>
         </a>
 		<a class="icon doctor_register" href = "princi.php">
-		    <div><br><img src="images/doctor.png"></div>
+		    <div><br><img src="images/principal.jpg" alt="Smiley face" height="150" width="150"> </div>
             <div class="logo d_registerl"></div>
             <div class="title d_registert">Principal Login</div>
-        </a>
-    </div>
+        </a>    </div>
 	</center>
 </body>
 </html>
